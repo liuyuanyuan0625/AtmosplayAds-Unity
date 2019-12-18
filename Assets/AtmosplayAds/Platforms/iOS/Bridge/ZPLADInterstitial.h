cb443b7b79a1b29b12fa3564960c26f7a689fb0c
JiaDingYi <wzy2010416033@163.com>
2019-12-18 14:56:26 +0800

    finetune interstitial bridge

--- Assets/AtmosplayAds/Platforms/iOS/Bridge/ZPLADInterstitial.h
+++ Assets/AtmosplayAds/Platforms/iOS/Bridge/ZPLADInterstitial.h
@@ -1,53 +1,30 @@
-//
-// Copyright (C) 2016 Google, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
 #import <Foundation/Foundation.h>
-#import <PlayableAds/PlayableAds.h>
+#import <AtmosplayAds/AtmosplayInterstitial.h>
 #import "ZPLADTypes.h"
 
-/// A wrapper around ZPLADInterstitial. Includes the ability to create ZPLADInterstitial objects,
+/// A wrapper around AtmosplayInterstitial. Includes the ability to create AtmosplayInterstitial objects,
 /// load them with ads, show them, and listen for ad events.
 @interface ZPLADInterstitial : NSObject
 
-/// Initializes a ZPLADInterstitial
+/// Initializes a AtmosplayInterstitial
 - (id)initWithInterstitialClientReference:(ZPLADTypeInterstitialClientRef *)interstitialClient
                                   adAppId:(NSString *)adAppId
                                  adUnitId:(NSString *)adUnitId;
 
 /// The interstitial ad.
-@property(nonatomic, strong) PlayableAds *interstitial;
-
+@property(nonatomic, strong) AtmosplayInterstitial *interstitial;
 /// A reference to the Unity interstitial client.
 @property(nonatomic, assign) ZPLADTypeInterstitialClientRef *interstitialClient;
-
 /// The ad received callback into Unity.
 @property(nonatomic, assign) ZPLADInterstitialDidReceivedAdCallback adReceivedCallback;
-
 /// The ad failed callback into Unity
 @property(nonatomic, assign) ZPLADInterstitialDidFailToReceiveAdWithErrorCallback adFailedCallback;
-
 /// The ad started playing callback into Unity.
 @property(nonatomic, assign) ZPLADInterstitialVideoDidStartPlayingCallback videoDidStartCallback;
-
 /// The ad "INSTALL" button is clicked callback into Unity.
 @property(nonatomic, assign) ZPLADInterstitiaDidClickCallback adClickedCallback;
-
 /// The ad was closed callback into Unity
 @property(nonatomic, assign) ZPLADInterstitialVideoDidCloseCallback adDidCloseCallback;
-
 /// The ad did complete callback into Unity.
 @property(nonatomic, assign) ZPLADInterstitialDidCompleteCallback videoDidCompleteCallback;
 
