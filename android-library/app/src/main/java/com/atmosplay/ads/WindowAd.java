816a428fd2ddbb72e9dec9195857f9e121c36f27
“yangfengbang” <yangfengbang@zplay.cn>
2020-04-07 11:55:38 +0800

    window Ad add hasHardwareAcceleration  check

--- android-library/app/src/main/java/com/atmosplay/ads/WindowAd.java
+++ android-library/app/src/main/java/com/atmosplay/ads/WindowAd.java
@@ -1,6 +1,8 @@
 package com.atmosplay.ads;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import com.atmosplayads.AtmosplayWindowAd;
@@ -12,12 +14,26 @@ public class WindowAd {
     private UnityWindowAdListener adListener;
     private AtmosplayWindowAd mWindowAd;
 
-    public WindowAd(Activity activity, String appId, String unitId, UnityWindowAdListener adListener) {
+    public WindowAd(Activity activity, String appId, String unitId, final UnityWindowAdListener adListener) {
         Log.d(TAG, "init: " + appId);
         this.mActivity = activity;
         this.adListener = adListener;
         if (mWindowAd != null) {
             mWindowAd.destroy();
+        }
+        Log.d(TAG, "hardwareAccelerated: " + hasHardwareAcceleration(activity));
+        if (!hasHardwareAcceleration(activity)) {
+            if (adListener != null) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (adListener != null) {
+                            adListener.onAdFailed("WiodnwAd need hardwareAccelerated, please at manifets set UnityPlayerActivity hardwareAccelerated is true ");
+                        }
+                    }
+                }).start();
+            }
+            return;
         }
         mWindowAd = new AtmosplayWindowAd(activity, appId, unitId);
         mWindowAd.setWindowAdListener(newWindowAdListener());
@@ -28,13 +44,18 @@ public class WindowAd {
         mActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                mWindowAd.setChannelId(channelId);
+                if (mWindowAd != null) {
+                    mWindowAd.setChannelId(channelId);
+                }
             }
         });
     }
 
     public boolean isLoaded() {
         Log.d(TAG, "isLoaded: " + mWindowAd.isReady());
+        if (mWindowAd == null) {
+            return false;
+        }
         return mWindowAd.isReady();
     }
 
@@ -44,7 +65,9 @@ public class WindowAd {
         mActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                mWindowAd.setPointAndWidth(mPointX, mPointY, width);
+                if (mWindowAd != null) {
+                    mWindowAd.setPointAndWidth(mPointX, mPointY, width);
+                }
             }
         });
     }
@@ -55,8 +78,10 @@ public class WindowAd {
         mActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if (mWindowAd.isReady()) {
-                    mWindowAd.show(mActivity);
+                if (mWindowAd != null) {
+                    if (mWindowAd.isReady()) {
+                        mWindowAd.show(mActivity);
+                    }
                 }
             }
         });
@@ -184,6 +209,33 @@ public class WindowAd {
                 }
             }
         });
+    }
+
+    // https://stackoverflow.com/a/18595681/7785373
+    private static boolean hasHardwareAcceleration(Activity activity) {
+        // Has HW acceleration been enabled manually in the current window?
+
+        // Window window = activity.getWindow();
+//        Log.d(TAG,"----window.getAttributes().flags: " + window.getAttributes().flags);
+//        Log.d(TAG,"----WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED: " + WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+//        if (window != null) {
+//            if ((window.getAttributes().flags
+//                    & WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED) != 0) {
+//                return true;
+//            }
+//        }
+
+        // Has HW acceleration been enabled in the manifest?
+        try {
+            ActivityInfo info = activity.getPackageManager().getActivityInfo(
+                    activity.getComponentName(), 0);
+            if ((info.flags & ActivityInfo.FLAG_HARDWARE_ACCELERATED) != 0) {
+                return true;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("Chrome", "getActivityInfo(self) should not fail");
+        }
+        return false;
     }
 
     public void onDestroy() {
